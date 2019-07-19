27853519b6d056d8bc498bd9b087cdd60d88e19b
Pete Mills <mills.pete@gmail.com>
2019-07-18 22:47:24 -0400

    added state machine framework to blink an LED

--- app.ino
+++ app.ino
@@ -1,9 +1,35 @@
-`void setup()
+void setup()
 {
-	
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+/* State Machine */
+typedef void (*fp)( void );
+fp machine_state = wakeup; /* Initial machine_state */
+
+
+/* Machine states */
+static void wakeup( void )
+{
+    machine_state = led_on;
+}
+
+static void led_on( void )
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(50); 
+    machine_state = led_off;
+}
+
+static void led_off( void )
+{
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);    
+    machine_state = led_on;
 }
 
 void loop()
 {
-	
+    /* Run the current machine_state */
+    machine_state();
 }
