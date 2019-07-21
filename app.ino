fb5cdb4c9286c548bcedf5c72f360d5c6e67c58d
Pete Mills <mills.pete@gmail.com>
2019-07-21 14:56:33 -0400

    adding ISR's and helper functions

--- app.ino
+++ app.ino
@@ -1,18 +1,19 @@
 /* Global Vars */
-uint8_t manual_override = 0;
-uint8_t motion_detected = 0;
+volatile uint8_t manual_override = 0;
+volatile uint8_t motion_detected = 0;
+volatile uint8_t system_armed = 0;
 uint16_t cycle_count = 0;
 
 /* Cycle times */
-uint8_t rearm_seconds = 10;
+uint32_t rearm_ms = 10000;
 uint16_t water_fill_ms = 2500;
 uint16_t air_actuation_ms = 300;
 
 /* GPIO pin defines */
-#define DRV__WATER      2   /* Water Solenoid Drive*/
-#define DRV__AIR        4   /* Air Solenoid Drive */
-#define _MON__OVERRIDE   7  /* Switch Input - Active Low */
-#define MON__MOTION     8   /* PIR Motion Detector */
+#define DRV__WATER      7   /* Water Solenoid Drive*/
+#define DRV__AIR        8   /* Air Solenoid Drive */
+#define _MON__OVERRIDE  2   /* Switch Input - Active Low */
+#define MON__MOTION     3   /* PIR Motion Detector */
 
 void setup()
 {
@@ -26,8 +27,79 @@ void setup()
 
     /* Digital Inputs */
     pinMode( MON__MOTION, INPUT );
+    attachInterrupt(digitalPinToInterrupt( MON__MOTION ), ISR__motion_detected, RISING );
     pinMode( _MON__OVERRIDE, INPUT_PULLUP );
+    attachInterrupt(digitalPinToInterrupt( _MON__OVERRIDE ), ISR__override, FALLING );
+}
 
+/* ISR__motion_detected
+* External interrupt called when
+* the PIR sensor is activated.
+  */
+void ISR__motion_detected( void )
+{
+    if( system_armed )
+    {
+        motion_detected = 1;
+    }
+}
+
+/* ISR__override
+* External interrupt called when 
+* a switch pulls the pin to GND.
+  */
+void ISR__override( void )
+{
+    if( system_armed )
+    {
+        manual_override = 1;
+    }
+}
+
+/* is_override
+* Tests if the manual override
+* flag is set.
+* This will also clear the flag.
+* 
+* @return 1 if set, else 0.
+  */
+uint8_t is_override( void )
+{
+    const uint8_t retval = manual_override == 1 ? 1 : 0;
+    manual_override = 0;
+    return retval;
+}
+
+/* sol_air_on
+* Turns ON the air solenoid.
+ */
+static void sol_air_on( void )
+{
+    digitalWrite( DRV__AIR, HIGH );
+}
+
+/* sol_air_off
+* Turns OFF the air solenoid.
+ */
+static void sol_air_off( void )
+{
+    digitalWrite( DRV__AIR, LOW );
+}
+
+/* sol_water_on
+* Turns ON the water solenoid.
+ */
+static void sol_water_on( void )
+{
+    digitalWrite( DRV__WATER, HIGH );
+}
+
+/* sol_water_off
+* Turns OFF the water solenoid.
+ */
+static void sol_water_off( void )
+{
+    digitalWrite( DRV__WATER, LOW );
 }
 
 /* State Machine */
@@ -37,9 +109,8 @@ fp machine_state = st_wakeup; /* Init the state machine */
 
 /* st_wakeup
 * A machine state for setting default parameters.
-*
  */
-static void wakeup( void )
+static void st_wakeup( void )
 {
     /* Print welcome banner. */
     Serial.println( "" );
@@ -55,7 +126,6 @@ static void wakeup( void )
 
 /* st_idle
 * A machine state to sit in waiting for motion to be detected.
-*
  */
 static void st_idle( void )
 {
@@ -65,15 +135,15 @@ static void st_idle( void )
         machine_state = st_led_on;
     }
 }
+
 /* st_led_on
 * A Machine state for turning ON the LED.
-*
  */
 static void st_led_on( void )
 {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(50); 
-    machine_state = led_off;
+    machine_state = st_led_off;
 }
 
 static void st_led_off( void )
