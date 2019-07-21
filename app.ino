a707e599b3bca2370859b188f9ac61bd77c7da75
Pete Mills <mills.pete@gmail.com>
2019-07-21 13:39:44 -0400

    adding serial support

--- app.ino
+++ app.ino
@@ -1,5 +1,9 @@
 void setup()
 {
+    /* Add serial support. */
+    Serial.begin(9600);
+
+    /* GPIO setup. */
     pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -14,6 +18,15 @@ fp machine_state = wakeup; /* Init the state machine */
  */
 static void wakeup( void )
 {
+    /* Print welcome banner. */
+    Serial.println( "" );
+    Serial.println( "  ___                _      ___ _    _      ___          _      _   _         " );
+    Serial.println( " | __|   _ __ _ _ _ ( )___ | __(_)__| |_   / __|_ __ _ _(_)_ _ | |_| |___ _ _ " );
+    Serial.println( " |   / || / _` | ' \|/(_-< | _|| (_-< ' \  \__ \ '_ \ '_| | ' \| / / / -_) '_|" );
+    Serial.println( " |_|_\\_, \__,_|_||_| /__/ |_| |_/__/_||_| |___/ .__/_| |_|_||_|_\_\_\___|_|  " );
+    Serial.println( "      |__/                                     |_|                            " );
+    Serial.println( "" );
+
     machine_state = led_on;
 }
 
